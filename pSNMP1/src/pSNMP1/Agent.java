9e7b7565ce0dfd0b8bc3f45dce5876e770f8780c
gmeye <gmeye@172.16.64.88>
2016-01-20 15:07:51 +0100

    AgentSimple+Agent

--- pSNMP1/src/pSNMP1/Agent.java
+++ pSNMP1/src/pSNMP1/Agent.java
@@ -1,5 +1,32 @@
 package pSNMP1;
 
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
 public class Agent {
+	
+public static void main (String args[]) {
+		
+		try {
+			LocateRegistry.createRegistry(1099);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			Naming.bind("server_rmi", (Remote) new Agentsimple());
+		} catch (MalformedURLException | RemoteException
+				| AlreadyBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+	}
 
 }
